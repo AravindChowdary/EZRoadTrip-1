3fb430ade3c17bb07ff9da18fac7a66c82af186d
dora <khievdora@gmail.com>
2017-07-29 16:35:29 -0500

    [IMPLEMENT]: Create Home, NewTrip, RouteSuggestion screen.

--- app/src/main/java/easyroadtrip/com/ezroadtrip/Presentation/activity/HomeActivity.java
+++ app/src/main/java/easyroadtrip/com/ezroadtrip/Presentation/activity/HomeActivity.java
@@ -6,26 +6,48 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import easyroadtrip.com.ezroadtrip.Presentation.Navigator.Navigator;
 import easyroadtrip.com.ezroadtrip.R;
 
+/**
+ *  HomeActivity will display all trip that we have been planed so far.
+ */
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.fabNewTrip)
+    FloatingActionButton mFabNewTrip;
+
+    @BindView(R.id.txtNoTrip)
+    TextView mTxtNoTrip;
+
+    @BindView(R.id.lstTrip)
+    ListView mLstTrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // TODO: Init Views
+        ButterKnife.bind(this);
+
+        mFabNewTrip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                // TODO: Navigate to NewTripActivity
+                Navigator.navigateToNewTripActivity(HomeActivity.this);
             }
         });
+
     }
 
 }
--- app/src/main/java/easyroadtrip/com/ezroadtrip/Presentation/dialog/CustomDatePickerDialog.java
+++ app/src/main/java/easyroadtrip/com/ezroadtrip/Presentation/dialog/CustomDatePickerDialog.java
@@ -1,8 +1,48 @@
 package easyroadtrip.com.ezroadtrip.Presentation.dialog;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
 /**
  * Created by Dora on 7/29/2017.
  */
 
-public class CustomDatePickerDialog {
+public class CustomDatePickerDialog extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+    private DatePickerDialogListener mListener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //return super.onCreateDialog(savedInstanceState);
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        if (mListener == null) {
+            throw new NullPointerException("View must implement DatePickerDialogListener!");
+        }
+        mListener.onDateSelected(year + "-" + month + "-" + dayOfMonth);
+    }
+
+    public void setDatePickerDialogListener(DatePickerDialogListener listener) {
+        this.mListener = listener;
+    }
+
+    public interface DatePickerDialogListener {
+        void onDateSelected(String date);
+    }
 }
